f53079eb1baa59ae2fc152f45b06a5e0ac73b040
Stiven Rodriguez <judrobinx@gmail.com>
2020-12-15 20:12:45 -0500

    fix (request): dd name space to path url fro reuest to microservice products

--- src/main/java/com/example/items/requests/interfaces/IProductRequest.java
+++ src/main/java/com/example/items/requests/interfaces/IProductRequest.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product-service", url = "localhost:4000")
+@FeignClient(name="product-service", url = "localhost:4000/products")
 public interface IProductRequest {
   @GetMapping()
   public List<Product> getProducts();
