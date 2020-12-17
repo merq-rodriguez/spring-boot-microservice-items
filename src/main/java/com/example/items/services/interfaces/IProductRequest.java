c09c98b7b1760c1ffdf332af8db160d23862c300
Stiven Rodriguez <judrobinx@gmail.com>
2020-12-17 15:25:36 -0500

    fix (): add namespace for request in controllers using feign

--- src/main/java/com/example/items/services/interfaces/IProductRequest.java
+++ src/main/java/com/example/items/services/interfaces/IProductRequest.java
@@ -2,15 +2,16 @@ package com.example.items.services.interfaces;
 
 import java.util.List;
 import com.example.items.models.Product;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product-service", url = "localhost:4000/products")
+@FeignClient(name="product-service"/* , url = "localhost:4000/products" */)
 public interface IProductRequest{
-  @GetMapping()
+  @GetMapping("/products")
   public List<Product> getProducts();
   
-  @GetMapping("{id}")
+  @GetMapping("/products/{id}")
   public Product findProductById(@PathVariable Long id);
 }
