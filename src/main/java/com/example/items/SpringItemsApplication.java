0a09d508bae8b7438337e74751df947b1ecd64fb
Stiven Rodriguez <judrobinx@gmail.com>
2020-12-17 15:29:18 -0500

    feat (main): add enable eureka client decorator

--- src/main/java/com/example/items/SpringItemsApplication.java
+++ src/main/java/com/example/items/SpringItemsApplication.java
@@ -2,10 +2,13 @@ package com.example.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+
+@EnableEurekaClient
 @EnableFeignClients
+@SpringBootApplication()
 public class SpringItemsApplication {
 
 	public static void main(String[] args) {
